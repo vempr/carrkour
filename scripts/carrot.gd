7edbd41b04e2a6fcdb15292303c183cb845d37ae
Alex <alexnguyena6@gmail.com>
2025-09-23 16:00:48 +0200

    player: carrot v1.0, all animations + jump buffering for enjoyable gameplay

--- scripts/carrot.gd
+++ scripts/carrot.gd
@@ -1,30 +1,62 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+const JUMP_BUFFER_TIME = 0.15
+const COYOTE_TIME = 0.1
+
+var flip_cooldown = 0.0
+var jump_buffer = 0.0
+var coyote_time = 0.0
 
 
 func _physics_process(delta: float) -> void:
 	if Globals.dead == true:
+		%Sprite.play("dead")
+		%Sprite.flip_h = false
 		velocity += get_gravity() * delta
 		move_and_slide()
 		return
-	
-	# Add the gravity.
-	if not is_on_floor():
+
+	if not is_on_floor(): 
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer = JUMP_BUFFER_TIME
+	else:
+		jump_buffer -= delta
+		
+	if is_on_floor():
+		coyote_time = COYOTE_TIME
+	else:
+		coyote_time -= delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	if jump_buffer > 0 and coyote_time > 0:
+		velocity.y = JUMP_VELOCITY
+		jump_buffer = 0
+		coyote_time = 0 
+
+	var direction := Input.get_axis("move_left", "move_right")
+
+	if not is_on_floor():
+		%Sprite.play("walk")
+		flip_cooldown -= delta
+		if flip_cooldown <= 0.0:
+			%Sprite.flip_h = !%Sprite.flip_h
+			flip_cooldown = 0.1
+	elif direction == 0:
+		%Sprite.play("idle")
+	elif direction == 1:
+		%Sprite.play("walk")
+		%Sprite.flip_h = false
+	else:
+		%Sprite.play("walk")
+		%Sprite.flip_h = true
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var dead = false
+var deaths = 0
+var suns = 0
